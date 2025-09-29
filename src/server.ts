d49dfdfeb8bc9d1fb20dff5dfb4467e22700e264
Yakov <yakov.varenik@gmail.com>
2025-09-29 16:02:24 +0300

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import http from 'http';
 import { handleRequest } from './routes';
-
-const PORT = 3000;
+import { config as envConfig } from 'dotenv';
 
 const server = http.createServer(handleRequest);
+
+const result = envConfig();
+
+if (result.error || !result.parsed) {
+  throw new Error(`File .env is missing or invalid: ${result.error}`);
+}
+
+const config = result.parsed;
+
+const PORT = config['PORT'];
 
 server.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
